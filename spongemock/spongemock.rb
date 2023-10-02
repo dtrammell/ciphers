6beaffdc54dc45f2641c3be1d0655a7dcd01712a
Dustin D. Trammell <dtrammell@dustintrammell.com>
2023-10-01 20:13:44 -0400

    Added encode_ascii method.

--- spongemock/spongemock.rb
+++ spongemock/spongemock.rb
@@ -2,38 +2,57 @@
 
 class SpongemockEncoder
 
-	# Encode
-	def encode( covertext, message )
+	# Encode ASCII
+	def encode_ascii( covertext, message )
 		# binary = message.unpack('b*').first # LSB
 		binary = message.unpack('B*').first # MSB
+		self.encode_bin( covertext, binary)
+	end
+
+	# Encode Binary
+	def encode_bin( covertext, binary )
 		puts "Message Binary: #{binary}"
 		binary = binary.split('')
 
+		puts "WARNING: Not enough cover text to encode all binary!" if covertext.length < binary.length
+
 		ciphertext = ''
 
-		# Encode each binary bit into a letter in the covertext
+		covercount = 0
 		bincount = 0
+
+		# Encode each binary bit into a letter in the covertext
 		covertext.split('').each do |c|
-#puts "#{c} / #{binary[bincount]}"
-			if c =~ /[a-zA-Z]/  # Only use letters to encode binary
-				if binary[bincount] == '1'
+			puts "#{c} / #{binary[bincount]}" if $DEBUG
+
+			# Reset bincount to loop binary if we've reached the end
+			bincount = 0 if binary[bincount] == nil
+
+			# Only use letters to encode binary
+			if c =~ /[a-zA-Z]/
+				case binary[bincount]
+				when '1'
 					# 1 equals uppercase
 					ciphertext << c.upcase
-				else
-					# 0 (or missing) equals lowercase
+				when '0'
+					# 0 equals lowercase
 					ciphertext << c.downcase
+				else
+					# nil or other remains the same
+					ciphertext << c
 				end
 
 				# Increment the binary array counter
 				bincount += 1
+				# Increment the covertext counter
+				covercount += 1
 			else
 				# Not a letter
 				ciphertext << c
 			end
-
 		end
 
-		puts "WARNING:  Not enough cover text to encode all binary!" if bincount < binary.length
+		puts "WARNING: Binary repeated but covertext ended before full repititon." if bincount != binary.length 
 
 		return ciphertext
 	end
@@ -60,7 +79,14 @@ puts "Covertext:  \"#{covertext}\""
 #puts "Bitstring:   #{first_bitstring}"
 
 # Encode
-ciphertext = spongemock.encode( covertext, cleartext )
+if cleartext.match? /^[01]*$/
+	# Binary
+	puts "Binary message detected..."
+	ciphertext = spongemock.encode_bin( covertext, cleartext )
+else
+	# Assume ASCII
+	ciphertext = spongemock.encode_ascii( covertext, cleartext )
+end
 puts "Ciphertext: \"#{ciphertext}\""
 
 # Decode
